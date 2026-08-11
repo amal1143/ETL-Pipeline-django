e330cd462dd47950bf2e49aa59742699c8e854c1
amal <amalchenthamara20@gmail.com>
2026-08-11 09:27:37 +0530

    Fix requirements.txt encoding, add vercel.json and api/index.py

---
+++ api/index.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ETLPipeproject.settings')
+
+from django.core.wsgi import get_wsgi_application
+app = get_wsgi_application()
